335d62f68953428c1637a45615d8f4aebe9479b5
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-06-19 16:08:21 +0200

    Initial code commit

---
+++ src/bootstrap.php
@@ -0,0 +1,7 @@
+<?php declare(strict_types=1);
+
+use Doctrine\Common\Annotations\AnnotationRegistry;
+
+AnnotationRegistry::registerLoader(function (string $class) {
+    return class_exists($class);
+});
